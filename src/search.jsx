bdea771f5e7cebcd12cef4cef8ce98ef76347d08
emiljdahm <emiljdahm@gmail.com>
2025-01-18 08:17:42 -0800

    returned api functionallity, modules confirmed working, need to solve data storage seems to be correct code? use state can be used to log changes

--- src/search.jsx
+++ src/search.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useEffect,useState } from 'react'
 import {getAuth} from './utilities'
+import './search.css'
 
-function Search(props){
 
-const[accessToken, setAccessToken] = useState("")
+function Search(){
+
+const [savedData, setSavedData] = ([])
+const[token, setAccessToken] = useState("")
 const[search, setSearch] = useState("")
 
+
+//get auth returns token code 
   useEffect(() => {
     const fetchToken = async () => {
       try{
@@ -18,6 +23,41 @@ const[search, setSearch] = useState("")
     };
     fetchToken()
   },[])
+//check if data is saved into state rerender
+  useEffect(() =>{
+    console.log(savedData)
+  }), [savedData]
+
+  //use token code to search for artist
+  async function SearchArtist() {
+
+    const searchParams = {
+      method: 'GET',
+      headers: {
+          'Authorization' : 'Bearer ' + token,
+          'Content-Type' : 'application/x-www-form-urlencoded'
+      } /* ,
+      body: 
+      'grant_type=client_credentials&client_id='+clientId+"&client_secret="+clientSecret */
+  }
+
+    const response = await fetch(`https://api.spotify.com/v1/search?`+`q=${search}&type=artist`,searchParams)
+    try{
+      if(!response.ok){
+        throw new Error ("Error Searching:")
+      } const data = await response.json();
+      // error occurs here cannot save data? maybe api limitation
+        console.log(data)
+      
+  
+      } catch (error){ console.log('Search Error:', error)}
+      
+    } 
+
+
+
+
+
 
   const handleSearch = (e) => {
     setSearch(e.target.value)
@@ -26,8 +66,9 @@ const[search, setSearch] = useState("")
   const handleButton = (e) => {
     e.preventDefault()
     setSearch(search)
-    console.log(`Searching for ${search}`)
-    setSearch("")
+    SearchArtist()
+
+  
   }
 
 
@@ -36,12 +77,13 @@ const[search, setSearch] = useState("")
 
     <div className="searchContainer">
         <h1>Search for Music</h1>
+        <form onSubmit={handleButton} id="search">
         <input type="text" placeholder="Search for music" onChange={handleSearch}></input>
         <button onChange={handleButton}>Search</button>
+        </form>
     </div>
     
     </>)
 
 }
-
 export default Search;
